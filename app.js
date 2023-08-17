c713e3810c0b0abc1a31667f2fdfc1987b43a18d
LongtProd <132977078+LongtProd@users.noreply.github.com>
2023-08-17 10:45:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,66 @@
-bootstrapStyle()
-
-
-function bootstrapStyle(){
+//bootstrapStyle()
+async function bootstrapStyle(){
+    return new Promise((resolve, reject) => {
     var link = document.createElement('link');
     var surl = 'vendor/bootstrap/css/bootstrap.min.css';
     link.href = surl;
     link.rel="stylesheet"
-    document.head.appendChild(link);
-    fontawesomeStyle()
+    resolve(document.head.appendChild(link))
+    });
 }
 
-function fontawesomeStyle(){
+async function fontawesomeStyle(){
+    await bootstrapStyle()
+    return new Promise((resolve, reject) => {
     var link = document.createElement('link');
     var surl = 'assets/css/fontawesome.css';
     link.href = surl;
     link.rel="stylesheet"
-    document.head.appendChild(link);
-    templatemoStyle()
+    resolve(document.head.appendChild(link))
+    });
 }
-function templatemoStyle(){
+async function templatemoStyle(){
+    await fontawesomeStyle()
+    return new Promise((resolve, reject) => {
     var link = document.createElement('link');
     var surl = 'assets/css/templatemo-574-mexant.css';
     link.href = surl;
     link.rel="stylesheet"
-    document.head.appendChild(link);
-    owlStyle()
+    resolve(document.head.appendChild(link))
+    });
 }
 
-function owlStyle(){
+async function owlStyle(){
+    await templatemoStyle()
+    return new Promise((resolve, reject) => {
     var link = document.createElement('link');
     var surl = 'assets/css/owl.css';
     link.href = surl;
     link.rel="stylesheet"
-    document.head.appendChild(link);
-    swiperStyle()
+    resolve(document.head.appendChild(link))
+    });
 }
 
-function swiperStyle(){
+async function swiperStyle(){
+    await owlStyle()
+    return new Promise((resolve, reject) => {
+    var link = document.createElement('link');
     var link = document.createElement('link');
     var surl = 'https://unpkg.com/swiper@7/swiper-bundle.min.css';
     link.href = surl;
     link.rel="stylesheet"
-    document.head.appendChild(link);
-    appfScript()
+    resolve(document.head.appendChild(link))
+    });
 }
 
-function appfScript(){
+async function appfScript(){
+    await swiperStyle()
+    return new Promise((resolve, reject) => {
     var script = document.createElement('script');
     var surl = 'appf.js';
     script.src = surl;
-    document.body.appendChild(script);
+    resolve(document.body.appendChild(script))
+    });
 }
+
+appfScript();
--- appf.js
+++ appf.js
@@ -1,5 +1,5 @@
-headPage();
 function headPage() {
+    return new Promise((resolve, reject) => {
     var header = document.getElementById("header");
     var xhr = new XMLHttpRequest();
             var url = '_head.html';
@@ -8,17 +8,21 @@ function headPage() {
             if (xhr.status === 200) {
                 // Set the innerHTML property of the div element to display the fetched content
                 //console.log(xhr.response)
-                header.innerHTML = xhr.response;
-                footPage();
+                resolve( header.innerHTML = xhr.response)
+               
+                //footPage();
             }
             else {
-                console.log('Request failed.  Returned status of ' + xhr.status);
+               reject(console.log('Request failed.  Returned status of ' + xhr.status))
             }
             };
             xhr.send();
+        });
 }
 
-function footPage() {
+async function footPage() {
+    await headPage()
+    return new Promise((resolve, reject) => {
     var footer = document.getElementById("footer");
     var xhr = new XMLHttpRequest();
             var url = '_foot.html';
@@ -27,80 +31,174 @@ function footPage() {
             if (xhr.status === 200) {
                 // Set the innerHTML property of the div element to display the fetched content
                 //console.log(xhr.response)
-                footer.innerHTML = xhr.response;
-                turnonbody()
+                resolve(footer.innerHTML = xhr.response)
+                //turnonbody()
             }
             else {
-                console.log('Request failed.  Returned status of ' + xhr.status);
+                reject(console.log('Request failed.  Returned status of ' + xhr.status))
             }
             };
             xhr.send();
+        });
 }
-function turnonbody() {
-    document.getElementById("mainbody").style.display = '';
-    jqueryScript()
+async function turnonbody() {
+    await footPage()
+    return new Promise((resolve, reject) => {
+    resolve(document.getElementById("mainbody").style.display = '')
+    var d = new Date(); 
+    document.getElementById("mahcryear").innerHTML = d.getFullYear();
+    });
 }
-jqueryScript()
-    function jqueryScript(){
+
+async function jqueryScript(){
+    await turnonbody()
+    return new Promise((resolve, reject) => {
         var script = document.createElement('script');
         var surl = 'vendor/jquery/jquery.min.js';
         script.src = surl;
-        document.body.appendChild(script);
-        bootstrapScript()
-    }
+        resolve(document.body.appendChild(script))
+    });
+}
     
-    function bootstrapScript(){
-        var script = document.createElement('script');
-        var surl = 'vendor/bootstrap/js/bootstrap.min.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        isotopeScript()
+async function bootstrapScript(){
+    await jqueryScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'vendor/bootstrap/js/bootstrap.min.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function isotopeScript(){
+    await bootstrapScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'assets/js/isotope.min.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function owlScript(){
+    await isotopeScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'assets/js/owl-carousel.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function tabsScript(){
+    await owlScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'assets/js/tabs.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function swiperScript(){
+    await tabsScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'assets/js/swiper.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function customScript(){
+    await swiperScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'assets/js/custom.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+async function sweeperoptionScript(){
+    await customScript()
+    return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    var surl = 'sweeperoption.js';
+    script.src = surl;
+    resolve(document.body.appendChild(script))
+    });
+}
+
+
+async function dosofink() {
+    await sweeperoptionScript()
+    return new Promise((resolve, reject) => {
+    var getcoolloc = window.location.href
+    var currento = getcoolloc.substr(getcoolloc.lastIndexOf('/') + 1);
+    if(currento == "team.html"){
+        function functeam() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/team.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+        function funcmedia() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/media.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+            functeam()
+            .then(funcmedia)
+    }else if(currento == "contact.html"){
+        function funccontact() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/contact.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+        function funcmedia() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/media.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+            funccontact()
+            .then(funcmedia)
+    }else if(currento == "services.html"){
+       resolve('')
+    }else if(currento.includes("service")){
+        function funccontact() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/contact.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+        function funcmedia() {
+            return new Promise((resolve, reject) => {
+                var script = document.createElement('script');
+                var surl = 'assets/js/media.js';
+                script.src = surl;
+                resolve(document.body.appendChild(script))
+            });
+        }
+            funccontact()
+            .then(funcmedia)
+    }else{
+        resolve('')
     }
-    
-    function isotopeScript(){
-        var script = document.createElement('script');
-        var surl = 'assets/js/isotope.min.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        owlScript()
-    }
-    
-    function owlScript(){
-        var script = document.createElement('script');
-        var surl = 'assets/js/owl-carousel.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        tabsScript()
-    }
-    
-    function tabsScript(){
-        var script = document.createElement('script');
-        var surl = 'assets/js/tabs.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        swiperScript()
-    }
-    
-    function swiperScript(){
-        var script = document.createElement('script');
-        var surl = 'assets/js/swiper.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        customScript()
-    }
-    
-    function customScript(){
-        var script = document.createElement('script');
-        var surl = 'assets/js/custom.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        sweeperoptionScript()
-    }
-    
-    function sweeperoptionScript(){
-        var script = document.createElement('script');
-        var surl = 'sweeperoption.js';
-        script.src = surl;
-        document.body.appendChild(script);
-        
-    }
+    });
+}
+
+dosofink();
